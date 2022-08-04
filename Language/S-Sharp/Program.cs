293ff9f078de1581b37f84fc584cca8ecaf114d2
mineTomek <me@tomeklukomski.pl>
2022-08-04 17:49:14 +0200

    Initialize project
    Create all the files for basic project :computer:

---
+++ Language/S-Sharp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace S_Sharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
