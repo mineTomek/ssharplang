92f603d361ba305832d64243b18e391ba16eeb9c
mineTomek <me@tomeklukomski.pl>
2022-08-04 21:07:41 +0200

    Create Arrows.cs
    Short code for creating arrows pointing to some part of text.

---
+++ Language/S-Sharp/Arrows.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace S_Sharp
+{
+    public class Arrows
+    {
+        public string arrowsText;
+
+        public Arrows(string text, ssharp.Position posStart, ssharp.Position posEnd)
+        {
+            string result = "";
+
+            // Calculate indices
+            int idxStart = Math.Max(text.LastIndexOf("\n", 0, posStart.idx), 0);
+        }
+    }
+}
