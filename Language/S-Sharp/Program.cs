89b6d83c659fa22ea7eafff9bbe5864751da4e88
mineTomek <me@tomeklukomski.pl>
2022-08-04 21:08:51 +0200

    Update Program.cs
    Basic reference to actual S# language compilator.

--- Language/S-Sharp/Program.cs
+++ Language/S-Sharp/Program.cs
@@ -6,7 +6,39 @@ namespace S_Sharp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            while (true) {
+                Console.Write("s# > ");
+                string code = Console.ReadLine();
+
+                if (code == "") { continue; }
+
+                Result res = ssharp.Run("<stdin>", code);
+
+                Console.WriteLine(res.ToString());
+            }
+        }
+    }
+
+    public class Result
+    {
+        public ssharp.Error error;
+        public string text;
+
+        public Result(ssharp.Error _error, string _text)
+        {
+            error = _error;
+            text = _text;
+        }
+
+        public override string ToString()
+        {
+            if (error == null)
+            {
+                return text;
+            } else
+            {
+                return error.ToString();
+            }
         }
     }
 }
